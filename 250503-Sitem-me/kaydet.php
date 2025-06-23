972eb72c090b61df0db4b382a4a8bdf5552d5e13
Rukiye <fotorukiyeb1544@gmail.com>
2025-06-24 01:06:50 +0300

    Add files via upload

--- 250503-Sitem-me/kaydet.php
+++ 250503-Sitem-me/kaydet.php
@@ -1,1 +1,30 @@
-Metniniz alıcıya iletilmiştir bizi tercih edip geri bildirim gönderdiğiniz için teşekkür ederiz...
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+  <meta charset="UTF-8">
+  <title>Kaydet Sayfası</title>
+
+  <!-- Google Analytics izleme kodu başlangıcı -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-M3CQ1THY3B"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag(){dataLayer.push(arguments);}
+
+  gtag('js', new Date());
+
+  gtag('config', 'G-M3CQ1THY3B');
+
+</script>
+<!-- Google Analytics izleme kodu sonu -->
+
+
+
+</head>
+<body>
+
+  <h1>Kaydet Sayfası</h1>
+  <p>Bu sayfa başarıyla kaydedildi.</p>
+
+</body>
+</html>
